13919409720b239865bd0b91231467a1256ff7a2
Malysheva Ksenia <maly6eva.ksenia@gmail.com>
2026-05-04 16:05:13 +0300

    refactor header

--- src/verstka/Header/Header.tsx
+++ src/verstka/Header/Header.tsx
@@ -1,26 +1,31 @@
 import miro from "../../../img/miro.svg"
 import frame from "../../../img/frame.svg"
+import style from "./Header.module.css"
 
 export const Header = () => {
     return (
-    <header>
-     <ul>
-         <li>
-             <img src={miro} alt="img"/>
-         </li>
-         <li>Продукт</li>
-         <li>Решения</li>
-         <li>Решения</li>
-         <li>Предприятия</li>
-         <li>Тарифы</li>
-     </ul>
-        <ul>
-            <li>
-                <img src={frame} alt="frame"/>
-            </li>
-            <button>Регистрация</button>
-        </ul>
-    </header>
+        <header className={style.header}>
+            <div className={`${style.container} ${style.header__inner}`}>
+                <div className={style.header__left}>
+                    <img src={miro} alt="img"/>
+
+                    <nav>
+                        <ul>
+                            <li className={style.li}>Продукт</li>
+                            <li>Решения</li>
+                            <li>Решения</li>
+                            <li>Предприятия</li>
+                            <li>Тарифы</li>
+                        </ul>
+                    </nav>
+
+                    <div className={style.header__right}>
+                        <img src={frame} alt="frame"/>
+                        <button>Регистрация</button>
+                    </div>
+                </div>
+            </div>
+        </header>
     );
 };
 
